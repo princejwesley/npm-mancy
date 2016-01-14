7d8f5e35781ace3c4977144b73cc469487a09a25
Prince J Wesley <princejohnwesley@gmail.com>
2016-01-14 11:11:13 +0530

    detach from parent process

--- cli.js
+++ cli.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 require('child_process')
-  .spawn(require('./'), process.argv.slice(2), { stdio: 'inherit' })
-  .on('close', process.exit.bind(process));
+  .spawn(require('./'), process.argv.slice(2), { stdio: 'inherit', detached: true })
+  .on('close', process.exit.bind(process)).unref();
