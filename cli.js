38f2718180122d84be47257965c26380ddd729ee
Prince J Wesley <princejohnwesley@gmail.com>
2016-05-07 00:05:22 +0530

    :bug: command line argument issue

--- cli.js
+++ cli.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 require('child_process')
-  .spawn(require('./'), process.argv.slice(2), { stdio: 'inherit', detached: true })
+  .spawn(require('./'), ['.'].concat(process.argv.slice(2)), { stdio: 'inherit', detached: true })
   .on('close', process.exit.bind(process)).unref();
